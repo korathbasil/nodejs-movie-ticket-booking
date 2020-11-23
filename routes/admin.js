a11796bc36820ed4ed559f958e45116ea916c725
korathbasil <korathbasil@live.com>
2020-11-23 17:40:28 +0530

    admin sidebar nav done

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
-// Admin home
+// Dashboard
 router.get("/", (req, res) => {
-  res.render("admin/admin-home", { admin: true });
+  res.render("admin/dashboard", { admin: true });
+});
+// Theatre Mangement
+router.get("/theater", (req, res) => {
+  res.render("admin/theater-management", { admin: true });
+});
+// User Mangement
+router.get("/user", (req, res) => {
+  res.render("admin/user-management", { admin: true });
+});
+// User Activity Tracker
+router.get("/user-activity", (req, res) => {
+  res.render("admin/user-activity", { admin: true });
 });
 
 module.exports = router;
