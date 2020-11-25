e9ea5315c25450f25547601f3faf7a110fdc406e
korathbasil <korathbasil@live.com>
2020-11-25 23:45:39 +0530

    theater sidebar nav added, all four urls set

--- routes/theater.js
+++ routes/theater.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
+// Dashboard
 router.get("/", (req, res) => {
   res.render("theater/dashboard", { theaterRoute: true });
+});
+// Movie Mangement
+router.get("/movie", (req, res) => {
+  res.render("theater/movie-management", { theaterRoute: true });
+});
+// Screens
+router.get("/screens", (req, res) => {
+  res.render("theater/screens", { theaterRoute: true });
+});
+// User Activity Tracker
+router.get("/user-activity", (req, res) => {
+  res.render("theater/user-activity", { theaterRoute: true });
 });
 
 module.exports = router;
