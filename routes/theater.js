9cba78523b71bb88cb332901d987ebd23c76cdaf
korathbasil <korathbasil@live.com>
2020-12-06 20:17:49 +0530

    added titles to theater routes, and added condition for theater sidebar

--- routes/theater.js
+++ routes/theater.js
@@ -3,28 +3,51 @@ const router = express.Router();
 
 // Login
 router.get("/login", (req, res) => {
-  res.render("theater/login", { theaterRoute: true });
+  res.render("theater/login", {
+    theaterRoute: true,
+    title: "Login - Theater Owner - Cinemax",
+  });
 });
 router.post("/login", (req, res) => {});
 // Dashboard
 router.get("/", (req, res) => {
-  res.render("theater/dashboard", { theaterRoute: true });
+  res.render("theater/dashboard", {
+    theaterRoute: true,
+    title: "Dashboard - Cinemax",
+    theaterOwner: true,
+  });
+});
+// Screen Management
+router.get("/screen", (req, res) => {
+  res.render("theater/screen-management", {
+    theaterRoute: true,
+    title: "Screen Management - Cinemax",
+    theaterOwner: true,
+  });
 });
 // Movie Mangement
 router.get("/movie", (req, res) => {
-  res.render("theater/movie-management", { theaterRoute: true });
-});
-// Screens
-router.get("/screen", (req, res) => {
-  res.render("theater/screen-management", { theaterRoute: true });
+  res.render("theater/movie-management", {
+    theaterRoute: true,
+    title: "Movie Management - Cinemax",
+    theaterOwner: true,
+  });
 });
 // User Activity Tracker
 router.get("/user-activity", (req, res) => {
-  res.render("theater/user-activity", { theaterRoute: true });
+  res.render("theater/user-activity", {
+    theaterRoute: true,
+    title: "User Activity Tracker - Cinemax",
+    theaterOwner: true,
+  });
 });
 // My Account
 router.get("/account", (req, res) => {
-  res.render("theater/account", { theaterRoute: true });
+  res.render("theater/account", {
+    theaterRoute: true,
+    title: "My Account - Cinemax",
+    theaterOwner: true,
+  });
 });
 
 module.exports = router;
