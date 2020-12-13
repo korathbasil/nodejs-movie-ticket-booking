0f4e2ab3163d9efa56a419aaae655bc3d2c65361
korathbasil <korathbasil@live.com>
2020-12-13 23:21:47 +0530

    user css file added

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
+// Home Page
 router.get("/", function (req, res) {
-  res.render("index");
+  res.render("user/home");
 });
 
 module.exports = router;
