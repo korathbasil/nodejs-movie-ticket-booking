713147586b2ca78fe8708cefb58e1ba17061bb54
korathbasil <korathbasil@live.com>
2020-12-25 23:57:51 +0530

    added login and signup forms for user

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -2,9 +2,37 @@ const db = require("../config/dbConfig");
 const collections = require("../config/collections");
 const { ObjectID } = require("mongodb");
 
+const Razorpay = require("razorpay");
+const bcrypt = require("bcrypt");
+
 module.exports = {
   signup: (userData) => {
-    return new Promise((resolve, reject) => {});
+    return new Promise(async (resolve, reject) => {
+      userData.password = await bcrypt.hash(userData.password, 10);
+      db.getDb()
+        .collection(collections.USER_COLLECTION)
+        .insertOne(userData)
+        .then((data) => {
+          resolve(data.ops[0]._id);
+        });
+    });
+  },
+  verifySignupOtp: (userId, otp) => {
+    return new Promise(async (resolve, reject) => {
+      const selectedUSer = await db
+        .getDb()
+        .collection(collections.USER_COLLECTION)
+        .findOne({ _id: ObjectID(userId) });
+      if (!selectedUSer) {
+        reject({ message: "Incorrect OTP" });
+      } else {
+        if (otp == selectedUSer.otp) {
+          resolve(selectedUSer);
+        } else {
+          reject({ message: "IncorrectOTP" });
+        }
+      }
+    });
   },
   login: (userData) => {
     return new Promise((resolve, reject) => {});
@@ -20,13 +48,35 @@ module.exports = {
     });
   },
   getMovieById: (movieId) => {
-    return new Promise((resolve, reject) => {
-      db.getDb()
+    return new Promise(async (resolve, reject) => {
+      const movie = await db
+        .getDb()
         .collection(collections.MOVIE_COLLECTION)
-        .findOne({ _id: ObjectID(movieId) })
-        .then((movie) => {
-          resolve(movie);
-        });
+        .aggregate([
+          { $match: { _id: ObjectID(movieId) } },
+          {
+            $lookup: {
+              from: collections.SHOW_COLLECTION,
+              foreignField: "_id",
+              localField: "shows",
+              as: "shows",
+            },
+          },
+        ]);
+    });
+  },
+  payRazorpay: () => {
+    const instance = new Razorpay({
+      key_id: process.env.RAZORPAY_KEY_ID,
+      key_secret: process.env.RAZORPAY_KEY_SECRET,
+    });
+    let options = {
+      amount: 1000,
+      currency: "INR",
+      receipt: "order_rcptid_11",
+    };
+    instance.orders.create(options, function (err, order) {
+      console.log(order);
     });
   },
 };
--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,31 @@ var router = express.Router();
 
 const userHelpers = require("../helpers/user-helpers");
 
+// Signup
+router.get("/signup", (req, res) => {
+  res.render("user/signup");
+});
+router.post("/signup", (req, res) => {
+  const otp = Math.floor(Math.random() * 1000000);
+  req.body.otp = otp;
+  userHelpers.signup(req.body).then((id) => {
+    res.render("user/verifyOTP", { userId: id });
+  });
+});
+router.post("/signup/verifyOTP/:userId", (req, res) => {
+  userId = req.params.userId;
+  userHelpers
+    .verifySignupOtp(userId, req.body.otp)
+    .then((user) => {
+      req.session.user = user;
+      res.redirect("/");
+    })
+    .catch((e) => res.redirect("/signup"));
+});
+// Login
+router.get("/login", (req, res) => {
+  res.render("user/login");
+});
 // Home Page
 router.get("/", function (req, res) {
   userHelpers.getAllMovies().then((movies) => {
@@ -12,10 +37,6 @@ router.get("/", function (req, res) {
       user: true,
     });
   });
-});
-// Login
-router.get("/login", (req, res) => {
-  res.render("user/login");
 });
 // Movie page
 router.get("/movie/:id", (req, res) => {
@@ -31,6 +52,13 @@ router.get("/cinemas", (req, res) => {
 // Account page
 router.get("/account", (req, res) => {
   res.render("user/account", { user: true });
+});
+
+router.get("/test", (req, res) => {
+  res.render("user/testPayment");
+});
+router.post("/pay/razorpay", (req, res) => {
+  userHelpers.payRazorpay();
 });
 
 module.exports = router;
