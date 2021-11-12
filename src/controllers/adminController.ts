5f771725313193d0762de15e73753d289097de91
Bazil Korath <korathbasil@live.com>
2021-11-13 00:04:20 +0530

    make ts adminController

---
+++ src/controllers/adminController.ts
@@ -0,0 +1,17 @@
+import {Request, Response} from "express"
+
+import adminService from "../services/adminService";
+
+export default {
+    adminSignup: (req: Request, res: Response) {
+    adminService
+    .signup(req.body)
+    .then(() => {
+      req.session.admin = false;
+      res.redirect("/admin/login");
+    })
+    .catch(() => {
+      res.redirect("/admin/login");
+    });
+    }
+}
