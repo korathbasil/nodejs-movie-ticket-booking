5eb96307605cd5914dac68378784345aee6356b2
Bazil Korath <korathbasil@live.com>
2021-11-13 00:46:29 +0530

    edit: express req obj type

--- src/controllers/adminController.ts
+++ src/controllers/adminController.ts
@@ -1,17 +1,17 @@
-import {Request, Response} from "express"
+import { Request, Response } from "express";
 
 import adminService from "../services/adminService";
 
 export default {
-    adminSignup: (req: Request, res: Response) {
+  adminSignup: (req: Request, res: Response) => {
     adminService
-    .signup(req.body)
-    .then(() => {
-      req.session.admin = false;
-      res.redirect("/admin/login");
-    })
-    .catch(() => {
-      res.redirect("/admin/login");
-    });
-    }
-}
+      .signup(req.body)
+      .then(() => {
+        req.session.admin = false;
+        res.redirect("/admin/login");
+      })
+      .catch(() => {
+        res.redirect("/admin/login");
+      });
+  },
+};
