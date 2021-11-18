838d24abb5e5ee7b45065c8894ae11d841c9b6b4
Bazil Korath <korathbasil@live.com>
2021-11-19 00:55:19 +0530

    edit admin router file

--- src/controllers/adminController.ts
+++ src/controllers/adminController.ts
@@ -3,7 +3,7 @@ import { Request, Response } from "express";
 import adminService from "../services/adminService";
 
 export default {
-  adminSignup: (req: Request, res: Response) => {
+  postSignup: (req: Request, res: Response) => {
     adminService
       .signup(req.body)
       .then(() => {
@@ -13,5 +13,158 @@ export default {
       .catch(() => {
         res.redirect("/admin/login");
       });
+  },
+
+  getLogin: (req: Request, res: Response) => {
+    var flash = req.flash("message");
+    console.log(flash);
+    adminService
+      .findAdmin()
+      .then(() => {
+        // console.log(req.session.adminErrors);
+        res.render("admin/login", {
+          title: "Login - Admin - Cinemax",
+          adminRoute: true,
+          adminExists: true,
+          errorMessage: req.session.errorMessage,
+        }); // Admin exists, render login form. There would be only one admin
+        req.session.errorMessage = null;
+      })
+      .catch(() => {
+        res.render("admin/login", {
+          title: "Login - Admin - CineMax",
+          adminRoute: true,
+          adminExists: false,
+        }); // Admin doesn't exist, render signup form
+      });
+  },
+
+  getLogout: (req: Request, res: Response) => {
+    req.session.admin = null;
+    req.session.destroy();
+    req.logout();
+    res.redirect("/");
+  },
+
+  getDashboard: (req: Request, res: Response) => {
+    res.render("admin/dashboard", {
+      title: "Dashboard - Admin - CineMax",
+      adminRoute: true,
+      admin: req.session.admin,
+    });
+  },
+
+  getTheaters: (req: Request, res: Response) => {
+    const ownerId = req.params.ownerId;
+    adminService.getTheaterOwner(ownerId).then((ownerDetails) => {
+      res.render("admin/theaters", {
+        title: "Theater Management - Admin - CineMax",
+        adminRoute: true,
+        admin: req.session.admin,
+        ownerDetails: ownerDetails,
+      });
+    });
+  },
+
+  getAddTheater: (req: Request, res: Response) => {
+    adminService.getAllTheterOwners().then((owners) => {
+      res.render("admin/theater-management", {
+        title: "Theater Management - Admin - CineMax",
+        adminRoute: true,
+        admin: req.session.admin,
+        owners: owners,
+      });
+    });
+  },
+
+  getAddtheaterOwner: (req: Request, res: Response) => {
+    res.render("admin/add-theater-owner", {
+      title: "Theater Management - Admin - CineMax",
+      adminRoute: true,
+      admin: req.session.admin,
+    });
+  },
+
+  postAddtheaterOwner: async (req: Request, res: Response) => {
+    const image = req.files.image;
+    const fileExtension =
+      image.name.split(".")[image.name.split(".").length - 1]; // Getting file extension by splitting on extesion dot(.)
+    const fileName = new Date().toISOString() + "." + fileExtension; // Creating a new file name with new Date() and fileExtension
+    const imagePath = "/images/owners/" + fileName; // Setting the public path
+    req.body.image = imagePath;
+
+    const username =
+      req.body.email.split("@")[0] + Math.floor(Math.random() * 10000 + 1); // Generating a username with email address
+    const password = Math.random().toString(36).substring(7); // Generating password
+
+    adminService.sendAddTheaterOwnerMail(
+      req.body.email,
+      username,
+      password,
+      async (e) => {
+        if (e) {
+          res.redirect("/admin/theater/add-owner");
+        } else {
+          req.body.username = username;
+          req.body.password = password;
+          await sharp(req.files.image.data)
+            .resize({ width: 360 })
+            .toFile(`./public/images/owners/${fileName}`);
+          adminService
+            .addTheaterOwner(req.body)
+            .then(() => res.redirect("/admin/theater"))
+            .catch(() => res.redirect("/admin/theater/add-owner"));
+        }
+      }
+    );
+  },
+
+  getEditTheaterOwner: (req: Request, res: Response) => {
+    const ownerId = req.params.ownerId;
+    adminService.getTheaterOwner(ownerId).then((ownerDetails) => {
+      res.render("admin/edit-theater-owner", {
+        title: "Theater Management - Admin - CineMax",
+        adminRoute: true,
+        admin: req.session.admin,
+        ownerDetails: ownerDetails,
+      });
+    });
+  },
+
+  postEditTheaterOwner: (req: Request, res: Response) => {
+    const ownerId = req.params.ownerId;
+    // theaterHeplers.getOwnerById(ownerId)
+    adminService
+      .editTheaterOwner(req.body, ownerId)
+      .then(() => {
+        res.redirect("/admin/theater");
+      })
+      .catch(() => {
+        res.redirect("/admin/theater");
+      });
+  },
+
+  getDeleteTheaterOwner: (req: Request, res: Response) => {
+    const ownerId = req.params.ownerId;
+    adminService
+      .deleteTheaterOwner(ownerId)
+      .then(() => res.redirect("/admin/theater"))
+      .catch((e) => console.log(e));
+  },
+
+  getUserManagement: (req: Request, res: Response) => {
+    res.render("admin/user-management", {
+      title: "User Management - Admin - CineMax",
+      adminRoute: true,
+      admin: req.session.admin,
+    });
+  },
+
+  getUserActivityTracker: (req: Request, res: Response) => {
+    res.render("admin/user-activity", {
+      title: "User Activity Tracker - Admin - CineMax",
+      adminRoute: true,
+      admin: req.session.admin,
+    });
   },
 };
--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,53 +1,20 @@
-const express = require("express");
-
-import { Router, Request, Response } from "express";
-const router = Router();
-const sharp = require("sharp");
+import { Router } from "express";
 const passport = require("passport");
 
-const adminService = require("../services/adminService");
+import adminController from "src/controllers/adminController";
+
+const router = Router();
+
 const verifyLogout = require("../middlewares/verify-admin-logout");
 const verifylogin = require("../middlewares/verify-admin-login");
 const verifyAdmin = require("../middlewares/verify-admin");
 const clearSession = require("../middlewares/clear-session");
 
 // Signup
-router.post("/signup", (req: Request, res: Response) => {
-  console.log(req.body);
-  adminService
-    .signup(req.body)
-    .then(() => {
-      req.session.admin = false;
-      res.redirect("/admin/login");
-    })
-    .catch(() => {
-      res.redirect("/admin/login");
-    });
-});
+router.post("/signup", adminController.postSignup);
 // Login
-router.get("/login", verifyLogout, (req, res) => {
-  var flash = req.flash("message");
-  console.log(flash);
-  adminService
-    .findAdmin()
-    .then(() => {
-      // console.log(req.session.adminErrors);
-      res.render("admin/login", {
-        title: "Login - Admin - Cinemax",
-        adminRoute: true,
-        adminExists: true,
-        errorMessage: req.session.errorMessage,
-      }); // Admin exists, render login form. There would be only one admin
-      req.session.errorMessage = null;
-    })
-    .catch(() => {
-      res.render("admin/login", {
-        title: "Login - Admin - CineMax",
-        adminRoute: true,
-        adminExists: false,
-      }); // Admin doesn't exist, render signup form
-    });
-});
+router.get("/login", verifyLogout, adminController.getLogin);
+
 router.post(
   "/login",
   clearSession,
@@ -58,93 +25,35 @@ router.post(
   })
 );
 // Logout
-router.get("/logout", verifylogin, verifyAdmin, (req, res) => {
-  req.session.admin = null;
-  req.session.destroy();
-  req.logout();
-  res.redirect("/");
-});
+router.get("/logout", verifylogin, verifyAdmin, adminController.getLogout);
+
 // Dashboard
-router.get("/", verifylogin, verifyAdmin, (req, res) => {
-  res.render("admin/dashboard", {
-    title: "Dashboard - Admin - CineMax",
-    adminRoute: true,
-    admin: req.session.admin,
-  });
-});
+router.get("/", verifylogin, verifyAdmin, adminController.getDashboard);
+
 // Theatre Mangement
-router.get("/theater", verifylogin, verifyAdmin, (req, res) => {
-  adminService.getAllTheterOwners().then((owners) => {
-    res.render("admin/theater-management", {
-      title: "Theater Management - Admin - CineMax",
-      adminRoute: true,
-      admin: req.session.admin,
-      owners: owners,
-    });
-  });
-});
+router.get("/theater", verifylogin, verifyAdmin, adminController.getAddTheater);
+
 // View All theaters of a selected owner
 router.get(
   "/theater/view-theaters/:ownerId",
   verifylogin,
   verifyAdmin,
-  (req, res) => {
-    const ownerId = req.params.ownerId;
-    adminService.getTheaterOwner(ownerId).then((ownerDetails) => {
-      res.render("admin/theaters", {
-        title: "Theater Management - Admin - CineMax",
-        adminRoute: true,
-        admin: req.session.admin,
-        ownerDetails: ownerDetails,
-      });
-    });
-  }
+  adminController.getTheaters
 );
+
 // Add theater owner
-router.get("/theater/add-owner", verifylogin, verifyAdmin, (req, res) => {
-  res.render("admin/add-theater-owner", {
-    title: "Theater Management - Admin - CineMax",
-    adminRoute: true,
-    admin: req.session.admin,
-  });
-});
+router.get(
+  "/theater/add-owner",
+  verifylogin,
+  verifyAdmin,
+  adminController.getAddtheaterOwner
+);
+
 router.post(
   "/theater/add-owner",
   verifylogin,
   verifyAdmin,
-  async (req, res) => {
-    const image = req.files.image;
-    const fileExtension =
-      image.name.split(".")[image.name.split(".").length - 1]; // Getting file extension by splitting on extesion dot(.)
-    const fileName = new Date().toISOString() + "." + fileExtension; // Creating a new file name with new Date() and fileExtension
-    const imagePath = "/images/owners/" + fileName; // Setting the public path
-    req.body.image = imagePath;
-
-    const username =
-      req.body.email.split("@")[0] + Math.floor(Math.random() * 10000 + 1); // Generating a username with email address
-    const password = Math.random().toString(36).substring(7); // Generating password
-
-    adminService.sendAddTheaterOwnerMail(
-      req.body.email,
-      username,
-      password,
-      async (e) => {
-        if (e) {
-          res.redirect("/admin/theater/add-owner");
-        } else {
-          req.body.username = username;
-          req.body.password = password;
-          await sharp(req.files.image.data)
-            .resize({ width: 360 })
-            .toFile(`./public/images/owners/${fileName}`);
-          adminService
-            .addTheaterOwner(req.body)
-            .then(() => res.redirect("/admin/theater"))
-            .catch(() => res.redirect("/admin/theater/add-owner"));
-        }
-      }
-    );
-  }
+  adminController.postAddtheaterOwner
 );
 
 // Edit theater owner
@@ -152,58 +61,35 @@ router.get(
   "/theater/edit-owner/:ownerId",
   verifylogin,
   verifyAdmin,
-  (req, res) => {
-    const ownerId = req.params.ownerId;
-    adminService.getTheaterOwner(ownerId).then((ownerDetails) => {
-      res.render("admin/edit-theater-owner", {
-        title: "Theater Management - Admin - CineMax",
-        adminRoute: true,
-        admin: req.session.admin,
-        ownerDetails: ownerDetails,
-      });
-    });
-  }
+  adminController.getEditTheaterOwner
 );
-router.post("/theater/edit-owner/:ownerId", verifyAdmin, (req, res) => {
-  const ownerId = req.params.ownerId;
-  // theaterHeplers.getOwnerById(ownerId)
-  adminService
-    .editTheaterOwner(req.body, ownerId)
-    .then(() => {
-      res.redirect("/admin/theater");
-    })
-    .catch(() => {
-      res.redirect("/admin/theater");
-    });
-});
+
+router.post(
+  "/theater/edit-owner/:ownerId",
+  verifyAdmin,
+  adminController.postEditTheaterOwner
+);
+
 // Delete theater owner
 router.get(
   "/theater/delete-owner/:ownerId",
   verifylogin,
   verifyAdmin,
-  (req, res) => {
-    const ownerId = req.params.ownerId;
-    adminService
-      .deleteTheaterOwner(ownerId)
-      .then(() => res.redirect("/admin/theater"))
-      .catch((e) => console.log(e));
-  }
+  adminController.getDeleteTheaterOwner
 );
 // User Mangement
-router.get("/user", verifylogin, verifyAdmin, (req, res) => {
-  res.render("admin/user-management", {
-    title: "User Management - Admin - CineMax",
-    adminRoute: true,
-    admin: req.session.admin,
-  });
-});
+router.get(
+  "/user",
+  verifylogin,
+  verifyAdmin,
+  adminController.getUserManagement
+);
 // User Activity Tracker
-router.get("/user-activity", verifylogin, verifyAdmin, (req, res) => {
-  res.render("admin/user-activity", {
-    title: "User Activity Tracker - Admin - CineMax",
-    adminRoute: true,
-    admin: req.session.admin,
-  });
-});
+router.get(
+  "/user-activity",
+  verifylogin,
+  verifyAdmin,
+  adminController.getUserActivityTracker
+);
 
-module.exports = router;
+export default router;
