2d61c06e79ca21cfef81c04f6e29e595a7e4e5f4
Bazil Korath <korathbasil@live.com>
2021-12-02 00:31:06 +0530

    add theater model

---
+++ src/models/theaterModel.ts
@@ -0,0 +1,23 @@
+import { Document } from "mongodb";
+
+import { getCollection } from "../config/dbConfig";
+
+type TheaterAddress = {
+  city: string;
+  state: string;
+  pincode: string;
+};
+
+type TheaterOwner = {
+  name: string;
+  email: string;
+  phone: string;
+};
+
+export class Theater implements Document {
+  name: string;
+  adress: TheaterAddress;
+  owner: TheaterOwner;
+}
+
+export const users = getCollection<Theater>("theaters");
