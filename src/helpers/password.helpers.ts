523dd0b52d1c62341c11abde7460d6b8cb22d6fd
Bazil Korath <korathbasil@live.com>
2021-12-05 00:00:31 +0530

    add comparePssword helper

--- src/helpers/password.helpers.ts
+++ src/helpers/password.helpers.ts
@@ -1,9 +1,16 @@
-import { hash } from "bcrypt";
+import { hash, compare } from "bcrypt";
 
 export default {
   hashPassword: async (password: string) => {
     const SALT = 10;
     const hashedPAssword = await hash(password, SALT);
     return hashedPAssword;
+  },
+
+  comparePassword: (
+    password: string,
+    encruptedPssword: string
+  ): Promise<boolean> => {
+    return compare(password, encruptedPssword);
   },
 };
