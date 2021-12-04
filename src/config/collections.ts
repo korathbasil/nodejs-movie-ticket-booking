f2f11f9262ea2be8aff8351c515bc828fd37e6f5
Bazil Korath <korathbasil@live.com>
2021-12-05 02:19:19 +0530

    add some methods in show servic, add show helper

--- src/config/collections.ts
+++ src/config/collections.ts
@@ -1,9 +1,9 @@
-module.exports = {
+export default {
   ADMIN_COLLECTION: "admin",
   USER_COLLECTION: "users",
   MOVIE_COLLECTION: "movies",
   THEATER_COLLECTION: "theaters",
   OWNERS_COLLECTION: "owners",
   SCREEN_COLLECTION: "screens",
-  SHOW_COLLECTION: "shows",
+  SHOWS: "shows",
 };
