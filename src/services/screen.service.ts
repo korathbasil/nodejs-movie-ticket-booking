8aafa6e4037b410f928348be8898fa7ca4667e0d
Bazil Korath <korathbasil@live.com>
2021-12-05 02:27:47 +0530

    add some methods in screen service

--- src/services/screen.service.ts
+++ src/services/screen.service.ts
@@ -1,10 +1,29 @@
+import { ObjectId } from "mongodb";
+
 import { getCollection } from "config/dbConfig";
 import { Screen } from "models/Screen.model";
+import { Theater } from "models/theaterModel";
 
-const screenCollection = getCollection<Screen>("screens");
+const screenCollection = getCollection<Screen>("screens")!;
+const theaterCollection = getCollection<Theater>("theaters")!;
 
 export class ScreenService {
-  public static async getScreenById() {}
+  public static async getScreenByTheaterId(id: string) {
+    return theaterCollection
+      .aggregate([
+        { $match: { _id: new ObjectId(id) } },
+        {
+          $lookup: {
+            from: "screens",
+            foreignField: "_id",
+            localField: "screens",
+            as: "screens",
+          },
+        },
+      ])
+      .toArray();
+  }
+
   public static async getAllScreens() {}
   public static asyncaddScreen() {}
   public static async editScreen() {}
@@ -12,26 +31,6 @@ export class ScreenService {
 }
 
 export default {
-  getScreens: (ownerId) => {
-    return new Promise(async (resolve, reject) => {
-      const owner = await db
-        .getDb()
-        .collection(collections.OWNERS_COLLECTION)
-        .aggregate([
-          { $match: { _id: ObjectID(ownerId) } },
-          {
-            $lookup: {
-              from: collections.SCREEN_COLLECTION,
-              foreignField: "_id",
-              localField: "screens",
-              as: "screens",
-            },
-          },
-        ])
-        .toArray();
-      resolve(owner[0].screens);
-    });
-  },
   addScreen: (screenData, ownerId) => {
     return new Promise((resolve, reject) => {
       screenData.shows = [];
