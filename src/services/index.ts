269edd137ceec0240e57fa18d403d0d0f45b5495
Bazil Korath <korathbasil@live.com>
2021-12-14 03:39:04 +0530

    srv export edit

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,5 @@
 import userServices from "./user.service";
 import { AdminService } from "./admin.service";
+import { MovieService } from "./movie.service";
 
-export { userServices, AdminService };
+export { userServices, AdminService, MovieService };
