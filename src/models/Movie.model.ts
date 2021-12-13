0b0511654ec6ec8b6eee7712eba0c11f66eabb18
Bazil Korath <korathbasil@live.com>
2021-12-14 03:29:19 +0530

    edit movie model, ad movie fn

--- src/models/Movie.model.ts
+++ src/models/Movie.model.ts
@@ -4,6 +4,7 @@ import { getCollection } from "../config/dbConfig";
 
 export class Movie implements Document {
   title: string;
+  runtimeInMinutes: number;
 }
 
 export const movieCollection = getCollection<Movie>("movies")!;
--- src/services/movie.service.ts
+++ src/services/movie.service.ts
@@ -1,47 +1,38 @@
 import { ObjectId } from "mongodb";
 
-import { getCollection } from "config/dbConfig";
-import { Movie } from "models/Movie.model";
+import { Movie, movieCollection } from "models/Movie.model";
 
-const movieCollection = getCollection<Movie>("movies")!;
+type MovieData = {
+  title: string;
+  runtimeHr: number;
+  runtimeMin: number;
+};
 
 export default class MovieServices {
-  public static async getAllMovies(): Promise<Movie[]> {
-    return movieCollection.find().toArray();
-  }
+  public static async addMovie(movieData: MovieData) {
+    const newMovieData = {
+      title: movieData.title,
+      runtimeInMinutes: movieData.runtimeHr * 60 + movieData.runtimeMin,
+    } as Movie;
 
-  public static async addMovie() {}
+    return movieCollection.insertOne(newMovieData);
+  }
 
   public static async getMovieByID(id: string): Promise<Movie | null> {
     return movieCollection.findOne({ _id: new ObjectId(id) });
   }
+  public static async getAllMovies(): Promise<Movie[]> {
+    return movieCollection.find().toArray();
+  }
 
   public static async editMovie() {}
 
-  public static async deleteMovie(id: string) {
+  public static deleteMovie(id: string) {
     return movieCollection.deleteOne({ _id: new ObjectId(id) });
   }
 }
 
 export const services = {
-  addMovie: (movieDetails) => {
-    return new Promise((resolve, reject) => {
-      movieDetails.runtimeInMin =
-        parseInt(movieDetails.runtimeHr) * 60 +
-        parseInt(movieDetails.runtimeMin);
-      delete movieDetails.runtimeHr;
-      delete movieDetails.runtimeMin;
-      db.getDb()
-        .collection(collections.MOVIE_COLLECTION)
-        .insertOne(movieDetails)
-        .then(() => resolve())
-        .catch((e) => {
-          console.log(e);
-          reject();
-        });
-    });
-  },
-
   editMovie: (newData, movieId) => {
     return new Promise((resolve, reject) => {
       db.getDb()
