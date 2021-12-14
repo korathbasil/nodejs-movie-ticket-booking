ea2288e4c0b91c8d10cd13b795d84d4cbf0d6e7e
Bazil Korath <korathbasil@live.com>
2021-12-15 00:22:35 +0530

    edit theaterservic edit theater

--- src/services/theater.service.ts
+++ src/services/theater.service.ts
@@ -42,10 +42,6 @@ export class TheaterServices {
     return theaterCollection.insertOne(newTheaterData);
   }
 
-  public static async getTheaterByID(id: string): Promise<Theater | null> {
-    return theaterCollection.findOne({ _id: new ObjectId(id) });
-  }
-
   public static async login(theaterCredentials: {
     username: string;
     password: string;
@@ -62,237 +58,233 @@ export class TheaterServices {
     );
   }
 
+  public static async getTheaterByID(id: string): Promise<Theater | null> {
+    return theaterCollection.findOne({ _id: new ObjectId(id) });
+  }
+
   public static async getAllTheaters(): Promise<Theater[]> {
     return theaterCollection.find().toArray();
   }
 
-  public static async editTheaterDetails() {}
+  public static async editTheaterDetails(newData: any, theaterId: string) {
+    return theaterCollection.updateOne(
+      { _id: new ObjectId(theaterId) },
+      {
+        $set: newData,
+      }
+    );
+  }
 
-  public static async deleteTheater() {}
-
-  public static async getAllTheaters() {}
-
-  public static async getTheater() {}
-
-  public static async editTheater() {}
-
-  public static async deleteTheater() {}
+  public static async deleteTheater(theaterId: string) {
+    theaterCollection.deleteOne({ _id: new ObjectId(theaterId) });
+  }
 }
 
-export default {
-  sendAddTheaterOwnerMail: (
-    recieverId: string,
-    username: string,
-    password: string,
-    cb: any
-  ) => {
-    // Configure Nodemailer
-    const nodemailer = require("nodemailer");
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.GMAIL_ID,
-        pass: process.env.GMAIL_PASSWORD,
-      },
-    });
-    const mailBody = `<body style="font: 14px 'Lucida Grande', Helvetica, Arial, sans-serif">
-        <div
-          style="
-            width: 100%;
-            max-width: 700px;
-            margin: 0 auto;
-            box-shadow: 3px 3px 3px lightgrey;
-            padding: 20px;
-          "
-        >
-          <div style="width: 100%">
-            <h1
-              style="
-                margin-left: 20px;
-                margin-top: 20px;
-                margin-bottom: 20px;
-                font-size: 40px;
-                color: #2f3547;
-              "
-            >
-              CineMax
-            </h1>
-          </div>
-          <div
-            style="width: 100%; padding-left: 20px; padding-right: 20px"
-          >
-            <p style="margin-bottom: 30px">
-              Welcome to CineMax Family. Your account has successfully registered as a
-              theater owner in CineMax. We are very happy to get you onboard. Let's
-              grow together!!
-            </p>
-            <p>Your login credentials are given below</p>
-            <div
-              style="
-                width: 100%;
-                display: flex;
-                align-items: center;
-                justify-content: center;
-              "
-            >
-              <div
-                style="
-                  width: fit-content;
-                  padding: 30px;
-                  border-radius: 20px;
-                  background-color: lightgrey;
-                "
-              >
-                <p>Username  :   <strong>${username}</strong></p>
-                <p>Password  :   <strong>${password}</strong></p>
-              </div>
-            </div>
-      
-            <p>To login click <a href="">here</a></p>
-          </div>
-          <div class="footer" style="text-align: center">
-            <p style="margin-bottom: 10px">CIneMax ©</p>
-          </div>
-        </div>
-      </body>
-      `;
-    transporter
-      .sendMail({
-        from: process.env.GMAIL_ID,
-        to: recieverId,
-        subject: "Theater Owner Account Added - CineMax",
-        html: mailBody,
-      })
-      .then(() => cb())
-      .catch((e: any) => cb(e));
-  },
-  editTheaterOwner: (newData: any, ownerId: string) => {
-    return new Promise<void>(async (resolve, reject) => {
-      const theaterOwner = await db
-        .getDb()
-        .collection(collections.OWNERS_COLLECTION)
-        .findOne({ _id: ObjectID(ownerId) });
-      // Checking if the email is chnged
-      if (theaterOwner.email === newData.email) {
-        db.getDb()
-          .collection(collections.OWNERS_COLLECTION)
-          .updateOne(
-            { _id: ObjectID(ownerId) },
-            {
-              $set: newData,
-            }
-          )
-          .then(() => resolve())
-          .catch(() => reject());
-      } else {
-        // Email has changed, new username and password has to be generated and send to the new email
-        const username =
-          newData.email.split("@")[0] + Math.floor(Math.random() * 10000 + 1); // Generating a username with email address
-        const password = Math.random().toString(36).substring(7); // Generating password
+// export default {
+//   sendAddTheaterOwnerMail: (
+//     recieverId: string,
+//     username: string,
+//     password: string,
+//     cb: any
+//   ) => {
+//     // Configure Nodemailer
+//     const nodemailer = require("nodemailer");
+//     const transporter = nodemailer.createTransport({
+//       service: "gmail",
+//       auth: {
+//         user: process.env.GMAIL_ID,
+//         pass: process.env.GMAIL_PASSWORD,
+//       },
+//     });
+//     const mailBody = `<body style="font: 14px 'Lucida Grande', Helvetica, Arial, sans-serif">
+//         <div
+//           style="
+//             width: 100%;
+//             max-width: 700px;
+//             margin: 0 auto;
+//             box-shadow: 3px 3px 3px lightgrey;
+//             padding: 20px;
+//           "
+//         >
+//           <div style="width: 100%">
+//             <h1
+//               style="
+//                 margin-left: 20px;
+//                 margin-top: 20px;
+//                 margin-bottom: 20px;
+//                 font-size: 40px;
+//                 color: #2f3547;
+//               "
+//             >
+//               CineMax
+//             </h1>
+//           </div>
+//           <div
+//             style="width: 100%; padding-left: 20px; padding-right: 20px"
+//           >
+//             <p style="margin-bottom: 30px">
+//               Welcome to CineMax Family. Your account has successfully registered as a
+//               theater owner in CineMax. We are very happy to get you onboard. Let's
+//               grow together!!
+//             </p>
+//             <p>Your login credentials are given below</p>
+//             <div
+//               style="
+//                 width: 100%;
+//                 display: flex;
+//                 align-items: center;
+//                 justify-content: center;
+//               "
+//             >
+//               <div
+//                 style="
+//                   width: fit-content;
+//                   padding: 30px;
+//                   border-radius: 20px;
+//                   background-color: lightgrey;
+//                 "
+//               >
+//                 <p>Username  :   <strong>${username}</strong></p>
+//                 <p>Password  :   <strong>${password}</strong></p>
+//               </div>
+//             </div>
 
-        newData.username = username;
-        newData.password = await hash(password, 10);
+//             <p>To login click <a href="">here</a></p>
+//           </div>
+//           <div class="footer" style="text-align: center">
+//             <p style="margin-bottom: 10px">CIneMax ©</p>
+//           </div>
+//         </div>
+//       </body>
+//       `;
+//     transporter
+//       .sendMail({
+//         from: process.env.GMAIL_ID,
+//         to: recieverId,
+//         subject: "Theater Owner Account Added - CineMax",
+//         html: mailBody,
+//       })
+//       .then(() => cb())
+//       .catch((e: any) => cb(e));
+//   },
+//   editTheaterOwner: (newData: any, ownerId: string) => {
+//     return new Promise<void>(async (resolve, reject) => {
+//       const theaterOwner = await db
+//         .getDb()
+//         .collection(collections.OWNERS_COLLECTION)
+//         .findOne({ _id: ObjectID(ownerId) });
+//       // Checking if the email is chnged
+//       if (theaterOwner.email === newData.email) {
+//         db.getDb()
+//           .collection(collections.OWNERS_COLLECTION)
+//           .updateOne(
+//             { _id: ObjectID(ownerId) },
+//             {
+//               $set: newData,
+//             }
+//           )
+//           .then(() => resolve())
+//           .catch(() => reject());
+//       } else {
+//         // Email has changed, new username and password has to be generated and send to the new email
+//         const username =
+//           newData.email.split("@")[0] + Math.floor(Math.random() * 10000 + 1); // Generating a username with email address
+//         const password = Math.random().toString(36).substring(7); // Generating password
 
-        // Configure Nodemailer
-        const nodemailer = require("nodemailer");
-        const transporter = nodemailer.createTransport({
-          service: "gmail",
-          auth: {
-            user: process.env.GMAIL_ID,
-            pass: process.env.GMAIL_PASSWORD,
-          },
-        });
-        const mailBody = `<body style="font: 14px 'Lucida Grande', Helvetica, Arial, sans-serif">
-        <div
-          style="
-            width: 100%;
-            max-width: 700px;
-            margin: 0 auto;
-            box-shadow: 3px 3px 3px lightgrey;
-            padding: 20px;
-          "
-        >
-          <div style="width: 100%">
-            <h1
-              style="
-                margin-left: 20px;
-                margin-top: 20px;
-                margin-bottom: 20px;
-                font-size: 40px;
-                color: #2f3547;
-              "
-            >
-              CineMax
-            </h1>
-          </div>
-          <div
-            style="width: 100%; padding-left: 20px; padding-right: 20px"
-          >
-            <p style="margin-bottom: 30px">
-              Your registered email has chnaged. Your new Credentials are given below
-            </p>
-            <p>Your login credentials are given below</p>
-            <div
-              style="
-                width: 100%;
-                display: flex;
-                align-items: center;
-                justify-content: center;
-              "
-            >
-              <div
-                style="
-                  width: fit-content;
-                  padding: 30px;
-                  border-radius: 20px;
-                  background-color: lightgrey;
-                "
-              >
-                <p>Username  :   <strong>${username}</strong></p>
-                <p>Password  :   <strong>${password}</strong></p>
-              </div>
-            </div>
-      
-            <p>To login click <a href="">here</a></p>
-          </div>
-          <div class="footer" style="text-align: center">
-            <p style="margin-bottom: 10px">CIneMax ©</p>
-          </div>
-        </div>
-      </body>
-      `;
-        transporter
-          .sendMail({
-            from: process.env.GMAIL_ID,
-            to: newData.email,
-            subject: "Registered Email changed - CineMax",
-            html: mailBody,
-          })
-          .then(() => {
-            db.getDb()
-              .collection(collections.OWNERS_COLLECTION)
-              .updateOne(
-                { _id: ObjectID(ownerId) },
-                {
-                  $set: newData,
-                }
-              )
-              .then(() => resolve())
-              .catch(() => reject());
-          })
-          .catch(() => {
-            reject();
-          });
-      }
-    });
-  },
-  deleteTheaterOwner: (ownerId: string) => {
-    return new Promise<void>((resolve, reject) => {
-      db.getDb()
-        .collection(collections.OWNERS_COLLECTION)
-        .deleteOne({ _id: ObjectID(ownerId) })
-        .then(() => resolve())
-        .catch(() => reject());
-    });
-  },
-};
+//         newData.username = username;
+//         newData.password = await hash(password, 10);
+
+//         // Configure Nodemailer
+//         const nodemailer = require("nodemailer");
+//         const transporter = nodemailer.createTransport({
+//           service: "gmail",
+//           auth: {
+//             user: process.env.GMAIL_ID,
+//             pass: process.env.GMAIL_PASSWORD,
+//           },
+//         });
+//         const mailBody = `<body style="font: 14px 'Lucida Grande', Helvetica, Arial, sans-serif">
+//         <div
+//           style="
+//             width: 100%;
+//             max-width: 700px;
+//             margin: 0 auto;
+//             box-shadow: 3px 3px 3px lightgrey;
+//             padding: 20px;
+//           "
+//         >
+//           <div style="width: 100%">
+//             <h1
+//               style="
+//                 margin-left: 20px;
+//                 margin-top: 20px;
+//                 margin-bottom: 20px;
+//                 font-size: 40px;
+//                 color: #2f3547;
+//               "
+//             >
+//               CineMax
+//             </h1>
+//           </div>
+//           <div
+//             style="width: 100%; padding-left: 20px; padding-right: 20px"
+//           >
+//             <p style="margin-bottom: 30px">
+//               Your registered email has chnaged. Your new Credentials are given below
+//             </p>
+//             <p>Your login credentials are given below</p>
+//             <div
+//               style="
+//                 width: 100%;
+//                 display: flex;
+//                 align-items: center;
+//                 justify-content: center;
+//               "
+//             >
+//               <div
+//                 style="
+//                   width: fit-content;
+//                   padding: 30px;
+//                   border-radius: 20px;
+//                   background-color: lightgrey;
+//                 "
+//               >
+//                 <p>Username  :   <strong>${username}</strong></p>
+//                 <p>Password  :   <strong>${password}</strong></p>
+//               </div>
+//             </div>
+
+//             <p>To login click <a href="">here</a></p>
+//           </div>
+//           <div class="footer" style="text-align: center">
+//             <p style="margin-bottom: 10px">CIneMax ©</p>
+//           </div>
+//         </div>
+//       </body>
+//       `;
+//         transporter
+//           .sendMail({
+//             from: process.env.GMAIL_ID,
+//             to: newData.email,
+//             subject: "Registered Email changed - CineMax",
+//             html: mailBody,
+//           })
+//           .then(() => {
+//             db.getDb()
+//               .collection(collections.OWNERS_COLLECTION)
+//               .updateOne(
+//                 { _id: ObjectID(ownerId) },
+//                 {
+//                   $set: newData,
+//                 }
+//               )
+//               .then(() => resolve())
+//               .catch(() => reject());
+//           })
+//           .catch(() => {
+//             reject();
+//           });
+//       }
+//     });
+//   },
+// };
