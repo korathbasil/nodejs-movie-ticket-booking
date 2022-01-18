a71aa7795a192e5ca72b8203997d6e6ee84184e7
Bazil Korath <korathbasil@live.com>
2022-01-19 02:53:34 +0530

    fix: name property of password field in html

--- src/helpers/password.helpers.ts
+++ src/helpers/password.helpers.ts
@@ -1,8 +1,7 @@
 import { hash, compare } from "bcrypt";
 
 export const passwordHelpers = {
-  hashPassword: async (password: string) => {
-    const salt = "jibbrish";
+  hashPassword: async (password: string, salt: number | string = 10) => {
     return hash(password, salt);
   },
 
