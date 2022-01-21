67f95ca6ad2ede1a97de1648c334a534953e6cfb
Bazil Korath <korathbasil@live.com>
2022-01-22 01:51:10 +0530

    add form closing after login

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -36,7 +36,7 @@ export class UserController {
     // TODO: input validation
     try {
       const user = await UserService.login({ email, password });
-      // req.session.user = user;
+      req.session.user = user;
       return res.status(200).json({ success: true, user });
     } catch (e) {
       return res.status(400).json({ success: false, error: e.message });
